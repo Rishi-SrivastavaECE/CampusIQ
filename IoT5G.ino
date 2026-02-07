ebad3e1d96dc57e9697917514e8c554aaf2cb8d9
TigpsAyush12 <aayushgupta2004ag@gmail.com>
2026-02-08 05:27:27 +0530

    Esp code created

--- IoT5G.ino
+++ IoT5G.ino
@@ -1,71 +1,97 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include <DHT.h>
 
-#define DHTPIN D3     
-#define DHTTYPE DHT22 
+const char* ssid = "VITC-EVENT";
+const char* password = "Eve@07&08#$";
 
-#define TRIG_PIN D5   
-#define ECHO_PIN D6   
+const char* mqtt_server = "79a024032c3340f1b31ae7145332b97d.s1.eu.hivemq.cloud";
+const int mqtt_port = 8883;
+const char* mqtt_user = "campusiq";
+const char* mqtt_pass = "camPusiq@404";
 
-#define MQ5_DIGITAL_PIN D7  
-#define LDR_ANALOG_PIN A0   
+const char* ROOM = "A101";
+
+#define DHTPIN D3
+#define DHTTYPE DHT22
+#define MQ5_PIN D7
 
 DHT dht(DHTPIN, DHTTYPE);
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+    delay(500);
+    Serial.print(".");
+    attempts++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nWiFi Connected");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nWiFi FAILED");
+  }
+}
+
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("MQTT connecting...");
+    if (client.connect("ESP8266_A101", mqtt_user, mqtt_pass)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void publishSensor(const char* sensor, float value) {
+  StaticJsonDocument<128> doc;
+  doc["room"] = ROOM;
+  doc["sensor"] = sensor;
+  doc["value"] = value;
+  char buffer[128];
+  serializeJson(doc, buffer);
+  String topic = String("campus/") + ROOM + "/" + sensor;
+  client.publish(topic.c_str(), buffer);
+}
 
 void setup() {
   Serial.begin(115200);
-  
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(MQ5_DIGITAL_PIN, INPUT);
-  
+  pinMode(MQ5_PIN, INPUT);
   dht.begin();
-  
-  Serial.println("\n--- ESP8266 Sensor System Started ---");
-  delay(2000);
+  randomSeed(analogRead(A0));
+  setup_wifi();
+  espClient.setInsecure();
+  client.setServer(mqtt_server, mqtt_port);
 }
 
 void loop() {
-  float h = dht.readHumidity();
+  if (!client.connected()) reconnect();
+  client.loop();
+
   float t = dht.readTemperature();
+  float h = dht.readHumidity();
 
-  long duration, distance;
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = duration * 0.034 / 2;
+  int gasDigital = digitalRead(MQ5_PIN);
+  int gasStatus = (gasDigital == LOW) ? 1 : 0;
 
-  int lightIntensity = analogRead(LDR_ANALOG_PIN);
-  int gasAlarm = digitalRead(MQ5_DIGITAL_PIN);
+  int powerValue = random(200, 801);
 
-  Serial.println("-----------------------------");
+  if (!isnan(t)) publishSensor("temp", t);
+  if (!isnan(h)) publishSensor("humidity", h);
+  publishSensor("gas", gasStatus);
+  publishSensor("power", powerValue);
 
-  Serial.print("Temperature: ");
-  if (isnan(t)) Serial.print("Error");
-  else Serial.print(t);
-  Serial.println(" °C");
-
-  Serial.print("Humidity:    ");
-  if (isnan(h)) Serial.print("Error");
-  else Serial.print(h);
-  Serial.println(" %");
-
-  Serial.print("Distance:    ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  Serial.print("Light Level: ");
-  Serial.print(lightIntensity);
-  Serial.println(" (0-1024)");
-
-  Serial.print("Gas Status:  ");
-  if (gasAlarm == LOW) {
-    Serial.println("!!! DANGER - GAS DETECTED !!!");
-  } else {
-    Serial.println("Safe");
-  }
-
-  delay(1000);
+  delay(5000);
 }
